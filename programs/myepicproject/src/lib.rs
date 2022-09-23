42c037dc1c8f2c38fbfd3f4f58e6701691047d7c
Jacqline <jacqlinegeng@gmail.com>
2022-09-22 21:53:08 -0400

    store data in the solana program

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -7,9 +7,34 @@ pub mod myepicproject {
     use super::*;
 
     pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> Result <()> {
+      let base_account = &mut _ctx.accounts.base_account;
+      base_account.total_gifs = 0;
+      Ok(())
+    }
+
+    pub fn add_gif(_ctx: Context<AddGif>) -> Result <()> {
+      let base_account = &mut _ctx.accounts.base_account;
+      base_account.total_gifs += 1;
       Ok(())
     }
   }
   
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
